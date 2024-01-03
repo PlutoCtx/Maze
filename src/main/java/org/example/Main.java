672c3f51b35936fe40a5ddea2e16fdccf0138f06
PlutoCtx <chentingxian195467@163.com>
2024-01-03 16:34:56 +0800

    Maze game

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+/**
+ * @author chent
+ */
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        GameFrame frame = new GameFrame();
+        GamePanel panel = new GamePanel(frame);
+        frame.add(panel);
+        frame.setVisible(true);
     }
 }
